239e024ae6f664988b9cfa29e14fa804eb0640d3
LeFanDOst <lefandost@gmail.com>
2022-03-16 16:13:50 +0100

    Légères modifs.

--- OSSpecs/src/WindowsCpuInfos.rs
+++ OSSpecs/src/WindowsCpuInfos.rs
@@ -1,4 +1,7 @@
 #![target_os("windows")]
 #![allow(non_snake_case)]
 
-
+use windows::{
+	core::*, Data::Xml::Dom::*, Win32::Foundation::*, Win32::System::Threading::*,
+	Win32::UI::WindowsAndMessaging::*,
+};
