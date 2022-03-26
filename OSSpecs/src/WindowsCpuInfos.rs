907c2360865929f1bb0ceae09a0d1334928ae7ee
LeFanDOst <lefandost@gmail.com>
2022-03-26 12:51:46 +0100

    Léger nettoyage d'une partie du code source.

--- OSSpecs/src/WindowsCpuInfos.rs
+++ OSSpecs/src/WindowsCpuInfos.rs
@@ -1,16 +1,9 @@
-//#![target_os("windows")]
 #![cfg(target_os = "windows")]
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 
-//use wmi::{COMLibrary, WMIConnection};
 use wmi::*;
 use serde::Deserialize;
-
-//use std::fmt;
-
-/*let com_con = COMLibrary::new()?;
-let wmi_con = WMIConnection::new(com_con.into())?;*/
 
 #[derive(Deserialize, Debug)]
 pub struct Win32_Processor
@@ -21,9 +14,10 @@ pub struct Win32_Processor
 	pub Availability: u16,
 	pub Caption: String,
 	pub Characteristics: u32,
-	//#[serde(skip_serializing_if = "Option::is_none")]
-	//pub ConfigManagerErrorCode: Option<u32>,
-	//pub ConfigManagerUserConfig: bool,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub ConfigManagerErrorCode: Option<u32>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub ConfigManagerUserConfig: Option<bool>,
 	pub CpuStatus: u16,
 	pub CreationClassName: String,
 	pub CurrentClockSpeed: u32,
@@ -31,14 +25,17 @@ pub struct Win32_Processor
 	pub DataWidth: u16,
 	pub Description: String,
 	pub DeviceID: String,
-	//pub ErrorCleared: bool,
-	//pub ErrorDescription: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub ErrorCleared: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub ErrorDescription: Option<String>,
 	pub ExtClock: u32,
 	pub Family: u16,
-	//pub InstallDate: datetime::WMIDateTime,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub InstallDate: Option<datetime::WMIDateTime>,
 	pub L2CacheSize: u32,
-	//#[serde(skip_serializing_if = "Option::is_none")]
-	//pub L2CacheSpeed: Option<u32>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub L2CacheSpeed: Option<u32>,
 	pub L3CacheSize: u32,
 	pub L3CacheSpeed: u32,
 	#[serde(skip_serializing_if = "Option::is_none")]
@@ -81,133 +78,6 @@ pub struct Win32_Processor
 	#[serde(skip_serializing_if = "Option::is_none")]
 	pub VoltageCaps: Option<u32>,
 }
-
-/*impl fmt::Debug for Win32_Processor
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
-	{
-        /*let naive = NaiveDateTime::from_timestamp(self.time as i64, 0);
-        let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
-        write!(f, "[Header : \n version {:?} \n previous block hash {:?} \n merkle root {:?} \n time {:?} \n bits {:?} \n nonce {:?} \n]", self.version, self.previous_block_hash, self.merkle_root, datetime, self.bits, self.nonce)*/
-        write!(f,
-		"AddressWidth : {:?} \n
-Architecture : {:?} \n
-AssetTag : {:?} \n
-Availability : {:?} \n
-Caption : {:?} \n
-Characteristics : {:?} \n
-ConfigManagerErrorCode : {:?} \n
-ConfigManagerUserConfig : {:?} \n
-CpuStatus : {:?} \n
-CreationClassName : {:?} \n
-CurrentClockSpeed : {:?} \n
-CurrentVoltage : {:?} \n
-DataWidth : {:?} \n
-Description : {:?} \n
-DeviceID : {:?} \n
-ErrorCleared : {:?} \n
-ErrorDescription : {:?} \n
-ExtClock : {:?} \n
-Family : {:?} \n
-InstallDate : {:?} \n
-L2CacheSize : {:?} \n
-L2CacheSpeed : {:?} \n
-L3CacheSize : {:?} \n
-L3CacheSpeed : {:?} \n
-LastErrorCode : {:?} \n
-Level : {:?} \n
-LoadPercentage : {:?} \n
-Manufacturer : {:?} \n
-MaxClockSpeed : {:?} \n
-Name : {:?} \n
-NumberOfCores : {:?} \n
-NumberOfEnabledCore : {:?} \n
-NumberOfLogicalProcessors : {:?} \n
-OtherFamilyDescription : {:?} \n
-PartNumber : {:?} \n
-PNPDeviceID : {:?} \n
-PowerManagementCapabilities : {:?} \n
-PowerManagementSupported : {:?} \n
-ProcessorId : {:?} \n
-ProcessorType : {:?} \n
-Revision : {:?} \n
-Role : {:?} \n
-SecondLevelAddressTranslationExtensions : {:?} \n
-SerialNumber : {:?} \n
-SocketDesignation : {:?} \n
-Status : {:?} \n
-StatusInfo : {:?} \n
-Stepping : {:?} \n
-SystemCreationClassName : {:?} \n
-SystemName : {:?} \n
-ThreadCount : {:?} \n
-UniqueId : {:?} \n
-UpgradeMethod : {:?} \n
-Version : {:?} \n
-VirtualizationFirmwareEnabled : {:?} \n
-VMMonitorModeExtensions : {:?} \n
-VoltageCaps : {:?} \n
-===== \n",
-			self.AddressWidth,
-			self.Architecture,
-			self.AssetTag,
-			self.Availability,
-			self.Caption,
-			self.Characteristics,
-			self.ConfigManagerErrorCode,
-			self.ConfigManagerUserConfig,
-			self.CpuStatus,
-			self.CreationClassName,
-			self.CurrentClockSpeed,
-			self.CurrentVoltage,
-			self.DataWidth,
-			self.Description,
-			self.DeviceID,
-			self.ErrorCleared,
-			self.ErrorDescription,
-			self.ExtClock,
-			self.Family,
-			self.InstallDate,
-			self.L2CacheSize,
-			self.L2CacheSpeed,
-			self.L3CacheSize,
-			self.L3CacheSpeed,
-			self.LastErrorCode,
-			self.Level,
-			self.LoadPercentage,
-			self.Manufacturer,
-			self.MaxClockSpeed,
-			self.Name,
-			self.NumberOfCores,
-			self.NumberOfEnabledCore,
-			self.NumberOfLogicalProcessors,
-			self.OtherFamilyDescription,
-			self.PartNumber,
-			self.PNPDeviceID,
-			self.PowerManagementCapabilities,
-			self.PowerManagementSupported,
-			self.ProcessorId,
-			self.ProcessorType,
-			self.Revision,
-			self.Role,
-			self.SecondLevelAddressTranslationExtensions,
-			self.SerialNumber,
-			self.SocketDesignation,
-			self.Status,
-			self.StatusInfo,
-			self.Stepping,
-			self.SystemCreationClassName,
-			self.SystemName,
-			self.ThreadCount,
-			self.UniqueId,
-			self.UpgradeMethod,
-			self.Version,
-			self.VirtualizationFirmwareEnabled,
-			self.VMMonitorModeExtensions,
-			self.VoltageCaps,
-		)
-    }
-}*/
 
 pub fn getProcessorsInformationsWin32() -> Result<Vec<Win32_Processor>, std::io::Error>
 {
