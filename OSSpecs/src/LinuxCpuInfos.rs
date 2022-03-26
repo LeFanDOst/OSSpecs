8e4244ac29c1af264c0facdcd211d98c18973928
LeFanDOst <lefandost@gmail.com>
2022-03-26 16:40:05 +0100

    Récupération des informations des processeurs sous Linux en cours.

--- OSSpecs/src/LinuxCpuInfos.rs
+++ OSSpecs/src/LinuxCpuInfos.rs
@@ -2,6 +2,9 @@
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 
+use std::process::Command;
+use std::string::String;
+
 pub struct Linux_Processor
 {
 	pub Architecture: String,
@@ -17,12 +20,64 @@ pub struct Linux_Processor
 	pub CPU_family: u32,
 	pub Model: u32,
 	pub Stepping: u32,
-	pub CPU MHz: f32,
+	pub CPU_MHz: f32,
 	pub BogoMIPS: f32,
 	pub Virtualization: String,
-	pub L1d cache: String,
-	pub L1i cache: String,
-	pub L2 cache: String,
-	pub L3 cache: String,
-	pub NUMA node0 CPU(s): String,
+	pub L1d_cache: String,
+	pub L1i_cache: String,
+	pub L2_cache: String,
+	pub L3_cache: String,
+	pub NUMA_node0_CPU_s: String,
+}
+
+pub fn getProcessorsInformationsLinux() -> Result<(), std::io::Error>
+{
+	let output = Command::new("sh").arg("-c").arg("lscpu").output()?;
+	let lscpuCmd = output.stdout;
+	
+	let lscpuCmdStr = match std::str::from_utf8(lscpuCmd.as_slice()) {
+		Ok(tmp) => tmp,
+		Err(err) => {
+			println!("{:?}", err);
+			
+			return Err(std::io::Error::new(
+				std::io::ErrorKind::InvalidInput,
+				"ERROR : UTF8Error thrown (Conversion Processors Datas Into String).",
+			));
+		},
+	};
+	
+	println!("{:?}", lscpuCmdStr);
+	
+	let cpuInfosString = String::from(lscpuCmdStr);
+	let mut arrTemp : Vec<String> = Vec::new();
+	
+	for line in cpuInfosString.lines()
+	{
+		let mut lineTemp = String::from(line);
+		let mut character : char = '.';
+		
+		while character != ':'
+		{
+			character = lineTemp.remove(0);
+		}
+		
+		lineTemp.remove(0);
+		lineTemp.remove(0);
+		
+		while lineTemp.starts_with(" ")
+		{
+			lineTemp.remove(0);
+		}
+		
+		println!("{:?}", lineTemp);
+		
+		arrTemp.push(lineTemp);
+	}
+	
+	let mut res : Linux_Processor;
+	
+	res.Architecture = arrTemp[0].clone();
+	
+	return Ok(());
 }
--- OSSpecs_tests/src/main.rs
+++ OSSpecs_tests/src/main.rs
@@ -5,16 +5,25 @@ extern crate OSSpecs;
 use OSSpecs::OSDetection;
 #[cfg(target_os = "windows")]
 use OSSpecs::WindowsCpuInfos;
+#[cfg(target_os = "linux")]
+use OSSpecs::LinuxCpuInfos;
 
 fn main()
 {
 	if OSDetection::isLinux()
 	{
 		println!("We are on a Linux Distribution OS.");
+		
+		#[cfg(target_os = "linux")]
+		LinuxCpuInfos::getProcessorsInformationsLinux();
 	}
 	else if OSDetection::isWindows()
 	{
 		println!("We are on Windows.");
+		
+		/*let tabProcos = WindowsCpuInfos::getProcessorsInformationsWin32().unwrap();
+		
+		println!("{:?}", tabProcos);*/
 	}
 	else if OSDetection::isMacOS()
 	{
@@ -24,10 +33,6 @@ fn main()
 	{
 		println!("We are on an other OS. Please, check the \"OSDetection.rs\" file to see which OS can be detect with this crate.");
 	}
-	
-	let tabProcos = WindowsCpuInfos::getProcessorsInformationsWin32().unwrap();
-	
-	println!("{:?}", tabProcos);
 	
     println!("Hello, world!");
 }
